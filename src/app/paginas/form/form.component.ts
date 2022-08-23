f11c6efe04c6670eeac525115aceb0e9143ca79d
DenisWm <denismamoni@hotmail.com>
2022-08-23 18:48:18 -0400

    Requisição get via cep

---
+++ src/app/paginas/form/form.component.ts
@@ -0,0 +1,40 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Cep } from 'src/app/models/cep.interface';
+import { Cliente } from 'src/app/models/cliente.interface';
+import { CepServico } from 'src/app/servicos/cep.servico';
+
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.sass']
+})
+export class FormComponent implements OnInit {
+
+  constructor(private http:HttpClient) {
+   }
+
+  ngOnInit(): void {
+  }
+
+  public cliente:Cliente= {
+   
+  } as Cliente;
+  public cep : Cep | undefined= {} as Cep;
+
+  public salvar(){
+    console.log(this.cliente);
+
+  }
+
+  public async buscaViaCep(){
+    this.cep = await new CepServico(this.http).getViaCep(this.cliente.cep);
+    if(this.cep){
+      this.cliente.endereco= this.cep.logradouro;
+      this.cliente.bairro = this.cep.bairro;
+      this.cliente.cidade = this.cep.localidade;
+      this.cliente.estado = this.cep.uf;
+      
+    }
+  }
+}
